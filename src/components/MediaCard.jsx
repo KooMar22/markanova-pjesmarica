3d06c33d07ee73a18fa9ab150a19242e55df7049
KooMar <djhracak@gmail.com>
2024-10-04 21:00:16 +0200

    Initial commit

---
+++ src/components/MediaCard.jsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import { useAudioPlayer } from "../hooks/useAudioPlayer";
+import "../mediacard.css";
+import { timer } from "../utils/timer";
+
+const MediaCard = ({ musicNumber, setMusicNumber, setOpen, songs, open }) => {
+    const {
+        audioRef,
+        play,
+        volume,
+        setVolume,
+        currentTime,
+        duration,
+        repeat,
+        handlePlayingAudio,
+        changeCurrentTime,
+        handleNextPrev,
+        handleRepeat
+    } = useAudioPlayer(50, songs, musicNumber, setMusicNumber);
+
+    const [showVolume, setShowVolume] = useState(false); 
+    const [isMuted, setIsMuted] = useState(false);
+
+    const toggleVolume = () => setShowVolume(prev => !prev);
+
+    const handleMute = () => {
+        setIsMuted(prev => !prev);
+        // Restore volume if unmuted, otherwise mute
+        setVolume(isMuted ? 50 : 0); 
+    };
+
+    // Define songs info
+    const currentSong = songs[musicNumber] || {};
+    const artistPortrait = currentSong.artist ? currentSong.artist.portrait : "";
+    const songName = currentSong.name || "Unknown Title";
+    const artistName = currentSong.artist ? currentSong.artist.name : "Unknown Artist"; 
+    const songFilePath = currentSong.file_path || "";
+
+    // Apply "blurred" class when SongsList is open in portrait mode
+    const portraitClassName = `portrait ${open ? "blurred" : ""}`;
+    const detailsClassName = `details ${open ? "blurred" : ""}`;
+    const progressbarClassName = `progressbar ${open ? "blurred" : ""}`;
+    const timerClassName = `timer ${open ? "blurred" : ""}`;
+    const controlsClassName = `controls ${open ? "blurred" : ""}`;
+
+    // Ensure "centered" class for MediaCard when SongsList is closed on landscape mode
+    // Otherwise, when the list is open, shift the Card to the left
+    const cardClassName = `card ${open ? "shift-left" : "centered"}`
+
+    return (
+        <div className={cardClassName}>
+            <div className="nav">
+                <div className="icon-container">
+                    <i className="material-symbols-outlined"
+                        onClick={() => setOpen(prev => !prev)}>queue_music
+                    </i>
+                    <span className="tooltip-text">Music List</span>
+                </div>
+                {songs.length > 0 ? (
+                    <span>Now Playing {musicNumber + 1} / {songs.length}</span>
+                ) : (
+                    <span>Open Music List and Select a Genre</span>
+                )
+                }
+            </div>
+
+            <div className={portraitClassName}>
+                <img src={artistPortrait} alt={`Portrait of ${artistName}`} />
+            </div>
+            
+            <div className={detailsClassName}>
+                <p className="title">{songName}</p>
+                <p className="artist">{artistName}</p>
+            </div>
+
+            <div className={progressbarClassName}>
+                <input type="range" min={0} max={duration} value={currentTime}
+                    onChange={changeCurrentTime} />
+            </div>
+
+            <div className={timerClassName}>
+                <span>{timer(currentTime)}</span>
+                <span>{timer(duration)}</span>
+            </div>
+
+            <div className={controlsClassName}>
+                <div className="icon-container">    
+                    <i className="material-symbols-outlined" onClick={handleRepeat}>
+                        {repeat === "autoplay" ? "autoplay" :
+                            repeat === "repeat" ? "repeat" : "shuffle"}
+                    </i>
+                    <span className="tooltip-text">
+                        {repeat === "autoplay" ? "Normal" :
+                            repeat === "repeat" ? "Repeat" : "Shuffle"}
+                    </span>
+                </div>
+                <i className="material-symbols-outlined" id="prev"
+                    onClick={() => handleNextPrev(-1)}>skip_previous</i>
+                <div className="play" onClick={handlePlayingAudio}>
+                    <i className="material-symbols-outlined">
+                        {play ? "pause" : "play_arrow"}
+                    </i>
+                </div>
+                <i className="material-symbols-outlined" id="next"
+                    onClick={() => handleNextPrev(1)}>skip_next
+                </i>
+                <i className="material-symbols-outlined"
+                    onClick={toggleVolume}>volume_up
+                </i>
+                {showVolume && (
+                    <div className="volume">
+                        <i className="material-symbols-outlined" onClick={handleMute}>
+                            {isMuted ? "volume_off" : "volume_up"}
+                        </i>
+                        <input type="range" min={0} max={100} value={volume}
+                            onChange={e => setVolume(Number(e.target.value))} />
+                        <span>{volume}</span>
+                    </div>
+                )}
+            </div>
+
+            <audio 
+                src={songFilePath}
+                ref={audioRef}
+                onTimeUpdate={changeCurrentTime}
+            />
+        </div>
+    );
+};
+
+export default MediaCard;
---
+++ src/components/SongsList.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import "../songslist.css";
+import { timer } from "../utils/timer";
+import { useGenres } from "../services/useGenres";
+import { useSongs } from "../services/useSongs";
+
+const SongsList = ({ open, musicNumber, setMusicNumber, setSongs }) => {
+    // Handle selected genres state
+    const [selectedGenre, setSelectedGenre] = useState(null);
+    
+    // Fetch genres and songs
+    const { data: genres, isLoading: loadingGenres } = useGenres();
+    const { data: songs, isLoading: loadingSongs, error: songsError } = useSongs(selectedGenre);
+
+    useEffect(() => {
+        if (songs) {
+            // Update the songs
+            setSongs(songs);
+        }
+    }, [songs, setSongs]);
+
+    // Pause playback and reset the song when the genre changes
+    const handleGenreChange = (e) => {
+        setSelectedGenre(parseInt(e.target.value)); // Set new genre
+        // Reset music number
+        setMusicNumber(0);
+    };
+
+    if (loadingGenres) return (
+        <div className="loading-div">
+            <p className="loading">Loading genres...
+                <br/>
+                It may take a while if loading genre for the first time.
+            </p>
+        </div>
+    );
+    if (loadingSongs) return (
+        <div className="loading-div">
+            <p className="loading">Loading songs...
+                <br />
+                It may take a while if loading genre for the first time.
+            </p>
+        </div>
+    );
+    if (songsError) return (
+        <div className="loading-div">
+            <p className="loading-error">Error loading songs: {songsError.message}</p>
+        </div>
+    );
+
+    return (
+        <div className={`list ${open ? "show" : ""}`}>
+            <div className="header">
+                <div>
+                    <span>Music list</span>
+                </div>
+            </div>
+
+            {/* Genre selection dropdown */}
+            <div className="genre-selector-div">
+                <select className="genre-selector"
+                    onChange={handleGenreChange} 
+                    // Set the value to selected genre
+                    value={selectedGenre || ""} 
+                >
+                    <option value="" disabled>Select a Genre</option>
+                    {genres && genres.map((genre) => (
+                        <option key={genre.id} value={genre.id}>
+                            {genre.genre}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <ul>
+                {songs && songs.length > 0 ? (
+                    songs.map((song, index) => (
+                        <li key={song.id} 
+                            onClick={() => {
+                                // Set current song index
+                                setMusicNumber(index); 
+                            }}
+                            className={`${musicNumber === index ? "playing" : ""}`}>
+                            <div className="row">
+                                <span>{song.name}</span>
+                                <p>{song.artist ? song.artist.name : "Unknown Artist"}</p>
+                            </div>
+                            <span className="duration">{timer(song.duration)}</span>
+                        </li>
+                    ))
+                ) : (
+                    <p>No songs available for this genre.</p>
+                )}
+            </ul>
+        </div>
+    );
+};
+
+export default SongsList;
